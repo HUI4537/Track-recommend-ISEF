f2e6ce033c026e11d5c5870cbef203527117623a
HUI4537 <smj1027328@gmail.com>
2025-05-07 19:45:35 +0900

    yeah

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_from_directory,send_file
-from flask_mysqldb import MySQL
 import os
 import sqlite3
 import base64
